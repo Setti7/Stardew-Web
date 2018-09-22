6258a03d1b7a9d3d63451b8aaa2c41236a836c51
André Setti <ansetti7@gmail.com>
2018-09-22 04:35:06 -0300

    Atualizados requirements

--- StardewWeb/settings.py
+++ StardewWeb/settings.py
@@ -169,5 +169,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'nao.responda.python@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get('nao.responda.python_password')
+EMAIL_HOST_PASSWORD = '$enhagenerica321'
 EMAIL_USE_TLS = True
