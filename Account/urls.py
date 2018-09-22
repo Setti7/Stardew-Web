4b0c50f809c70c108d79c29bf1af83a76cff591a
André Setti <ansetti7@gmail.com>
2018-09-22 19:23:25 -0300

    Chnaged password reset to account recovery

--- Account/urls.py
+++ Account/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('create/', views.signup, name='create account'),
 
-    path('password-reset', auth_views.password_reset,
+    path('account-reset', auth_views.password_reset,
          {'html_email_template_name': 'registration/password_reset_email.html'}, name='password_reset'),
-    path('password-reset/done', auth_views.password_reset_done, name='password_reset_done'),
+    path('account-reset/done', auth_views.password_reset_done, name='password_reset_done'),
     path('password-reset/token/<uidb64>/<token>', auth_views.password_reset_confirm, name='password_reset_confirm'),
     path('password-reset/done', auth_views.password_reset_complete, name='password_reset_complete'),
 
